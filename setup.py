f5268388e04cbc7f6ed8f098d42c70f1648e1301
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-09-02 11:43:52 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='OSRS-Hiscores',  
+    version='0.6',
+    author="Matthew Thornton",
+    author_email="osrsbotdetector@gmail.com",
+    description="An Old School Runescape (OSRS) Hiscores Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThorntonMatthewD/osrs-hiscores",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
